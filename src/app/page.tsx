7f370f7b3358f1f1fefbe00d09fb40b734832a96
Sidharth Babu <95430324+sidhuiwnl@users.noreply.github.com>
2024-05-15 15:04:10 +0530

    schema setup and image ordering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,22 @@ import { db } from "~/server/db";
 
 export const dynamic = 'force-dynamic'
 
-const mockDataUrl = [
-  'https://utfs.io/f/5e119118-31ca-49f3-af8b-cd94bd80ed91-1ri2q1.jpg',
-  'https://utfs.io/f/7906bf55-1b0a-479b-a147-fac96615750c-kflh1x.jpg',
-  'https://utfs.io/f/8f4819eb-0445-40a9-ab51-b2a98839c3d5-4cglxh.jpg',
-  'https://utfs.io/f/4d56c9df-59f2-4f17-95a4-0cc25cecab28-kf7jw6.jpg'
-]
 
-const mockImages = mockDataUrl.map((url,index) =>({
-  id: index + 1,
-  url
-}))
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany() 
+  const mockImages = await db.query.image.findMany({
+    orderBy : (model,{desc}) => desc(model.id)
+  }) 
   
   return (
     <main className="">
       
      <div className="flex flex-wrap gap-4">
-
-     {posts.map((post) =>(
-      <div key={post.id}>{post.name}</div>
-     ))}
     
       {[...mockImages].map((image) =>(
-        <div key={image.id} className="w-48">
+        <div key={image.id} className="w-48 flex flex-col">
           <img src={image.url} />
-
+          <div>{image.name}</div>
 
         </div>
       ))}
