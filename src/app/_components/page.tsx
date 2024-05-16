d66d5156cac72e951e36c8fbcfe0c7546a2c5d9e
Sidharth Babu <95430324+sidhuiwnl@users.noreply.github.com>
2024-05-16 12:39:29 +0530

    Uploadthing added

--- src/app/_components/page.tsx
+++ src/app/_components/page.tsx
@@ -1,17 +1,25 @@
- import { SignedIn, SignedOut, UserButton,SignInButton } from "@clerk/nextjs"
+'use client';
+
+import { SignedIn, SignedOut, UserButton,SignInButton } from "@clerk/nextjs"
+import { UploadButton } from "~/utils/uploadthing"
+import { useRouter } from "next/navigation"
 
 export function NavBar(){
+  const router = useRouter()
     return (
       <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b">
         <div>Gallery</div>
        
   
   
-        <div>
+        <div className="flex flex-row">
             <SignedOut>
                 <SignInButton />
             </SignedOut>
             <SignedIn>
+                <UploadButton endpoint="imageUploader" onClientUploadComplete={() =>{
+                  router.refresh()
+                }}/>
                 <UserButton/>
             </SignedIn>
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "~/app/api/uploadthing/core";
 import { NavBar } from "./_components/page";
 import { GeistSans } from "geist/font/sans";
 import  { ClerkProvider } from "@clerk/nextjs"
+
 
 
 export const metadata = {
@@ -20,6 +25,15 @@ export default function RootLayout({
   return (
     <ClerkProvider> 
       <html lang="en" className={`${GeistSans.variable}`}>
+      <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
       <body className="flex flex-col gap-4">
         <NavBar/>
         {children}</body>
