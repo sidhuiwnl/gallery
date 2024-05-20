4ea15b5c51d527282e164f139e04e514b592743d
sidharthinfernal <sidharthinfernal@gmail.com>
2024-05-20 12:31:21 +0000

    add meta data for images

--- src/components/Full-page-image.tsx
+++ src/components/Full-page-image.tsx
@@ -1,4 +1,5 @@
 
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImages } from "~/server/queries";
 
 
@@ -7,6 +8,7 @@ export default async function FullpageView(props:{id:number})
  {
   
   const image = await getImages(props.id)
+  const uploaderId = await clerkClient.users.getUser(image.userId)
   return (
     <div className="flex w-full h-full">
       <div className="flex-shrink flex justify-center items-center">
@@ -17,8 +19,18 @@ export default async function FullpageView(props:{id:number})
       /> 
       </div>
        
-      <div className="flex w-48 flex-shrink-0 flex-col border-l">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l ">
+        <div className="text-center text-lg border-b p-3">{image.name}</div>
+
+
+        <div className="flex flex-col p-2">
+          <span>Uploaded By:</span>
+          <span>{uploaderId.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Created On:</span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
       </div>
     </div>
      
