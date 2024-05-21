a5c17fa0899aa3dc3b678e74eefe8ed02c1859ac
sidharthinfernal <sidharthinfernal@gmail.com>
2024-05-21 07:21:46 +0000

    changed the upload button wih neat ui

--- src/app/_components/page.tsx
+++ src/app/_components/page.tsx
@@ -1,25 +1,19 @@
-'use client';
-
 import { SignedIn, SignedOut, UserButton,SignInButton } from "@clerk/nextjs"
-import { UploadButton } from "~/utils/uploadthing"
-import { useRouter } from "next/navigation"
-
+import SimpleUploadButton from "./simple-upload-button";
 export function NavBar(){
-  const router = useRouter()
+ 
     return (
       <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b">
         <div>Gallery</div>
        
   
   
-        <div className="flex flex-row">
+        <div className="flex flex-row gap-4 items-center">
             <SignedOut>
                 <SignInButton />
             </SignedOut>
             <SignedIn>
-                <UploadButton endpoint="imageUploader" onClientUploadComplete={() =>{
-                  router.refresh()
-                }}/>
+                <SimpleUploadButton/>
                 <UserButton/>
             </SignedIn>
 
